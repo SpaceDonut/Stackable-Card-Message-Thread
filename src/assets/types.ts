1eafe7674ca5ab906cd9692830b4da6c404c4a38
MindHaze <martinkol3v@gmail.com>
2022-08-15 23:27:52 +0300

    [WIP] Remove boilerplate code and create first component RootPage.
    Add json of message threads to data.ts and types for it in types.ts

---
+++ src/assets/types.ts
@@ -0,0 +1,16 @@
+export interface Message {
+  subject_id: number;
+  question_id: number;
+  thread_id: string;
+  reply_to?: string;
+  text: string;
+  created_at: string;
+  subject: string;
+  question: string;
+  team: string;
+  id: string;
+  score?: number;
+  acknowledged?: boolean;
+}
+
+export type Threads = Message[][];
---
+++ src/components/RootPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { reactive } from "vue";
+import { THREADS } from "../assets/data";
+import { Threads } from "../assets/types";
+
+const threadList = reactive<Threads>(THREADS);
+</script>
+
+<template>
+  <div class="hello">
+    <h1>{{ threadList.length && threadList[0][0].question }}</h1>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss"></style>
