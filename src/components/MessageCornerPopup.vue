afad2185fdcda5ff660083ee2abd339fc3b4dc56
MindHaze <martinkol3v@gmail.com>
2022-08-17 09:07:51 +0300

    [WIP]Create MessageCornerPopup component and styles for it

---
+++ src/components/MessageCornerPopup.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps<{
+  messageNumber: number;
+}>();
+</script>
+
+<template>
+  <div class="message-corner-popup-container">
+    {{ props.messageNumber }}
+  </div>
+</template>
